c9b80b55dc609599a82f1a76c188fdd877ee7cdc
Justin Gert <justinvandergert@outlook.com>
2019-04-09 11:41:37 +0200

    Update Boat.cs

--- Unity Bestanden/New Unity Project (1)/Assets/Scripts/Boat.cs
+++ Unity Bestanden/New Unity Project (1)/Assets/Scripts/Boat.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 
 public class Boat : MonoBehaviour
 {
+    Rigidbody Rb;
+    bool Driving = true;
 
+    public float RotSpeed;
+    public float MovSpeed;
+    public float MaxSpeed;
 
 
     void Start()
     {
-        
+        Rb = GetComponent<Rigidbody>();
     }
     
     void Update()
     {
-        
+        if (Driving)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(0, -RotSpeed * Time.deltaTime, 0, Space.Self);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(0, RotSpeed * Time.deltaTime, 0, Space.Self);
+            }
+
+            if (Input.GetKey(KeyCode.W) && Rb.velocity.x < MaxSpeed)
+            {
+                Rb.AddForce(transform.forward * MovSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.S) && Rb.velocity.x > 0)
+            {
+                Rb.AddForce(-transform.forward * MovSpeed * Time.deltaTime);
+                if(Rb.velocity.x < 0)
+                {
+                    Rb.velocity = new Vector3(0, 0, 0);
+                }
+            }
+        }
     }
 }
